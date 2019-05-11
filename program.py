6980d8307c94b6d1c33d1ca6c34116669d89c834
Prakhar Shukla <prafiles@yahoo.com>
2019-05-12 01:41:14 +0530

    new method for calculation

--- program.py
+++ program.py
@@ -16,9 +16,7 @@ time.sleep(0.25) #Blink white 1 second to confirm LED function
 oledExp.driverInit(1)
 oledExp.setBrightness(0)
 oledExp.setTextColumns()
-rx_bytes=0
-tx_bytes=0
-timestamp = time.time()
+
 
 def color_blink(r,g,b,duration=0.25,sleep=0.25):
     #LED GPIO 1 means LOW and 0 means HIGH
@@ -30,6 +28,46 @@ def color_blink(r,g,b,duration=0.25,sleep=0.25):
     gpio_gled.setValue(1)
     gpio_bled.setValue(1)
     time.sleep(sleep)
+
+
+interface_readings_dict={}
+
+def get_interface_last_val(interface):
+    global interface_readings_dict
+    interface_readings = interface_readings_dict.get(interface, None)
+    if interface_readings is None:
+        interface_readings = {
+            'rx' : 0,
+            'tx' : 0,
+            'time' : time.time()
+        }
+        interface_readings_dict[interface] = interface_readings
+    return interface_readings
+
+def set_interface_value(interface, rx, tx, timestamp):
+    global interface_readings_dict
+    interface_readings_dict[interface] = {
+        'rx':rx,
+        'tx':tx,
+        'time': timestamp
+    }
+
+
+def get_interface_val(interface):
+    last_readings = get_interface_last_val(interface)
+    text = get('https://10.0.0.1/fetchif.cgi?' + interface, verify=False, auth=(os.environ['USER'],os.environ['PASSWORD'])).text.split('\n')[1]
+    text_cols = text.split()
+    rx_bytes_new = int (text_cols[1])
+    tx_bytes_new = int (text_cols[9])
+    timestamp_new = time.time()
+    set_interface_value(interface, rx_bytes_new, tx_bytes_new, timestamp_new)
+    rx_kilobyte_rate = int ((rx_bytes_new - last_readings['rx'])*8 / ((timestamp_new - last_readings['time']) * 1000))
+    tx_kilobyte_rate = int ((tx_bytes_new - last_readings['tx'])*8 / ((timestamp_new - last_readings['time']) * 1000))
+    return {
+        'rx_rate':rx_kilobyte_rate / 1000,
+        'tx_rate':tx_kilobyte_rate / 1000
+    }
+
 
 while True:
     time.sleep(5)
@@ -58,17 +96,8 @@ while True:
         color_blink(1,0,0)
         continue
     color_blink(0,1,0) #Internet Good
-    text = get('https://10.0.0.1/fetchif.cgi?ppp0',verify=False,auth=(os.environ['USER'],os.environ['PASSWORD'])).text.split('\n')[1]
-    timestamp_new = time.time()
-    text_cols = text.split()
-    rx_bytes_new = int (text_cols[1])
-    tx_bytes_new = int (text_cols[9])
-    rx_kilobyte_rate = int ((rx_bytes_new - rx_bytes)*8 / ((timestamp_new - timestamp) * 1000))
-    tx_kilobyte_rate = int ((tx_bytes_new - tx_bytes)*8 / ((timestamp_new - timestamp) * 1000))
-    rx_bytes = rx_bytes_new
-    tx_bytes = tx_bytes_new
-    timestamp = timestamp_new
+    readings = get_interface_val('ppp0')
     oledExp.setCursor(2,0)
-    oledExp.write("Rx " + str (rx_kilobyte_rate/1000) + " Mb/s")
+    oledExp.write("Rx " + str (readings['rx']) + " Mb/s")
     oledExp.setCursor(3,0)
-    oledExp.write("Tx " + str (tx_kilobyte_rate/1000) + " Mb/s")
+    oledExp.write("Tx " + str (readings['tx']) + " Mb/s")
