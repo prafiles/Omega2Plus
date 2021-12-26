05315ca8ba2e01a49000802f4b9913fe4c9045d0
Prakhar Shukla <prafiles@yahoo.com>
2021-12-27 03:29:25 +0530

    update for fixing with new domain

--- program.py
+++ program.py
@@ -89,7 +89,7 @@ while True:
 
     #Internet HTTP Try
     check_website('https://monitor.prafiles.in',"Personal",2)
-    check_website('https://nl-dev.solulever.com',"NL-Dev",3)
+    check_website('https://brabo-platform.nl-dev.solulever.com/',"NL-Dev",3)
     check_website('https://dev.solulever.com',"E2E",4)
     check_website('https://ep.solulever.com',"EP",5)
     check_website('https://lightscameraazadi.in',"LCA",6)
